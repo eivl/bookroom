5327eb163b09ed2225f23aacf3cb91d9339bfff1
Eivind Teig <eivind.teig@gmail.com>
2021-10-13 17:24:48 +0200

    Add Event app

---
+++ bookroom/event/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from model_utils.models import TimeFramedModel
+
+
+class Room(TimeFramedModel):
+    name = models.CharField('Room Name', max_length=255)
+
+
+class Event(TimeFramedModel):
+    title = models.CharField('Event Title', max_length=255)
+    all_day = models.BooleanField('All-Day event', default=False)
+    start = models.DateTimeField('Event start')
+    end = models.DateTimeField('Event end')
+    editable = models.BooleanField('Make the event editable', default=True)
+    overlap = models.BooleanField('Make the event overlapping', default=True)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
